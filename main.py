62e62c9a6813b4b8d7f8fe0998d9aa93e27c4435
Eleftherios Chaniotakis <lchanio@echyperion.com>
2026-06-10 00:09:11 +0300

    (fix): Add type hints and update character counts sort.

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-from stats import count_words, count_characters, sort_word_counts
+from stats import count_words, count_characters, sort_word_counts, chars_dict_to_sorted_list
 import sys
+import os
 
-def get_book_text(filepath):
+def get_book_text(filepath: str) -> str:
     with open(filepath, "r") as f:
         return f.read()
 
 def main():
     if len(sys.argv) != 2:
+        # book_path="./books/frankenstein.txt"
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
     
     book_path = sys.argv[1]
+        
+    if not os.path.exists(book_path):
+        print(f"Error: The file {book_path} does not exist.")
+        sys.exit(1)
 
     text = get_book_text(book_path)
     num_words = count_words(text)
@@ -19,6 +25,12 @@ def main():
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     char_counts = count_characters(text)
+    
+    # These lines were added to print the character counts for an earlier submission
+    # sorted_chars = chars_dict_to_sorted_list(char_counts)
+    # for item in sorted_chars:
+    #     print(item)
+    
     print("--------- Character Count -------")
     sorted = sort_word_counts(char_counts)
     for char, count in sorted:
--- stats.py
+++ stats.py
@@ -1,8 +1,8 @@
-def count_words(text):
+def count_words(text: str) -> int:
     words = text.split()
     return len(words)
 
-def count_characters(text):
+def count_characters(text: str) -> dict[str, int]:
     char_counts = {}
     for char in text.lower():
         if not char.isalpha():
@@ -10,5 +10,16 @@ def count_characters(text):
         char_counts[char] = char_counts.get(char, 0) + 1
     return char_counts
 
-def sort_word_counts(word_counts):
+def sort_word_counts(word_counts: dict[str, int]) -> list[tuple[str, int]]:
     return sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
+
+def sort_on(char_counts: tuple[str,int])->int:
+    return char_counts[1]
+
+def chars_dict_to_sorted_list(num_chars_dict: dict[str, int]) -> list[tuple[str,int]]:
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append((ch, num_chars_dict[ch]))
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
